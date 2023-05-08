d6aa94b33a18236b1a40a5e3af0a593425b243e9
pmaisterra <122354099+pmaisterra@users.noreply.github.com>
2023-05-08 23:51:20 +0200

    Add files via upload

---
+++ appimagendef5.py
@@ -0,0 +1,32 @@
+# Import
+import streamlit as st
+import pickle
+
+# Título de la página
+
+st.title('Clasificación de imágenes')
+st.markdown("Introduce una imagen y comprueba como se clasifica")
+
+def load_image():
+    uploaded_file = st.file_uploader(label='Pick an image to test')
+    if uploaded_file is not None:
+        image_data = uploaded_file.getvalue()
+        image= cv2.resize(image_data,(224, 224))
+        return image
+def load_model():
+    uploaded_file = st.file_uploader("Upload Model")
+    if uploaded_file is not None:
+      clf2 = pickle.loads(uploaded_file.read())
+      return clf2
+def main():
+    st.title('Image upload demo')
+    if st.button('Load Image to predict'):
+      imageto=load_image()
+    if st.button('Load Model'):
+      model=load_model()
+    if st.button('Image prediction'):
+      prediction =model.predict(imageto)
+      st.succes=prediction
+
+if __name__ == '__main__':
+    main()
